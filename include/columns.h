2672f7e1ca60e77d0c6b4b806f45d3219e034f4c
Maciej Zygadło <Xsardas665@gmail.com>
2025-04-27 12:15:06 +0000

    File include/columns.h added

---
+++ include/columns.h
@@ -0,0 +1,25 @@
+#ifndef COLUMNS_H
+#define COLUMNS_H
+
+#include <gtkmm.h>
+
+class ModelColumns : public Gtk::TreeModelColumnRecord {
+public:
+    ModelColumns() {
+        add(m_id); 
+        add(m_name); 
+        add(m_color); 
+        add(m_type); 
+        add(m_mana_cost); 
+        add(m_quantity);
+    }
+
+    Gtk::TreeModelColumn<int> m_id;
+    Gtk::TreeModelColumn<Glib::ustring> m_name;
+    Gtk::TreeModelColumn<Glib::ustring> m_color;
+    Gtk::TreeModelColumn<Glib::ustring> m_type;
+    Gtk::TreeModelColumn<Glib::ustring> m_mana_cost;
+    Gtk::TreeModelColumn<int> m_quantity;
+};
+
+#endif // COLUMNS_H
